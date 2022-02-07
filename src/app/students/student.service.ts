fc14be2c84353de74eea360255213a8800b6ddca
Mark Ortiz <ortizmark@hotmail.com>
2022-02-06 21:20:13 -0800

    first commit

---
+++ src/app/students/student.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../models/api-models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private baseApiUrl = "https://localhost:44351";
+
+  constructor(private httpClient: HttpClient ) { }
+
+  getStudents(): Observable<Student[]> {
+    return this.httpClient.get<Student[]>(this.baseApiUrl + '/api/Student');
+  }
+
+  getStudent(studentId: string): Observable<Student> {
+    return this.httpClient.get<Student>(this.baseApiUrl + '/api/Student/' + studentId);
+  }
+
+}
